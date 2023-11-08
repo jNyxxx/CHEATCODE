aaee9ba32343b498a7f8a6fbbc282d4636bab82a
nYx <147728991+jNyxxx@users.noreply.github.com>
2023-11-08 23:33:22 +0800

    Update project37.c

--- project37.c
+++ project37.c
@@ -3,19 +3,22 @@
 double myRoundedOffNum(double num);
 int main() 
 {
-    double decimalNumber, roundedOffNumber;
-  
+    double decimalNumber, roundedOffNumber; //function declare
+    
+    //input
     printf("Enter a floating point number to be rounded off: ");
     scanf("%lf", &decimalNumber);
     
+    //calling function
     roundedOffNumber = myRoundedOffNum(decimalNumber);
     
+    //output
     printf("Rounded number: %.6lf\n", roundedOffNumber);
     
     return 0;
 }
 
-double myRoundedOffNum(double num1) 
+double myRoundedOffNum(double num1) //function define
 {
     double roundedOffNumber, value;
     roundedOffNumber = num1 * 100 + 0.005; 
