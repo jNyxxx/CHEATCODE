72241b2619817c78048d9e0ec252745af9d6b8ab
nYx <147728991+jNyxxx@users.noreply.github.com>
2023-11-08 22:44:32 +0800

    project28.c

---
+++ project28.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void functionCallName(); //declaring function
+
+int main() {
+
+    functionCallName(); //calling the function
+
+    return 0;
+}
+
+void functionCallName() //function define
+{
+    printf("*****************************************\n");
+    printf("*\t\t\t\t\t\t\t\t\t\t*\n");
+    printf("*\t\t\tJunex Glenn A. Baran\t\t*\n");
+    printf("*\t\t\t\t\t\t\t\t\t\t*\n");
+    printf("*****************************************\n");
+}
