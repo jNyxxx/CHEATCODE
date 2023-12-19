6dc952abcabf0ad4d177d0a1ffe0137e29347052
nYx <147728991+jNyxxx@users.noreply.github.com>
2023-12-19 12:53:28 +0800

    Update pro53.c

--- pro53.c
+++ pro53.c
@@ -1,13 +1,22 @@
+/* This code determines the grade of the student.
+    Written by: Baran, Junex Glenn A.
+    Date: 11/24/23
+*/
+
 #include <stdio.h>
 
+//User-defined functions
 void scoresRead(int *test1, int *test2, int *test3, int *average, int *secondAndThirdAveScore);
 char gradeDecide(int average, int test3, int secondAndThirdAveScore);
 void showOutput(char grade);
 
+//Main function
 int main() {
+    //Local declarations | Declaring variables
     int firstTestScore, secondTestScore, thirdTestScore, average, secondAndThirdAveScore;
     char studentGrade;
 
+    //Calling the user-defined functions in the main function
     scoresRead(&firstTestScore, &secondTestScore, &thirdTestScore, &average, &secondAndThirdAveScore);
 
     studentGrade = gradeDecide(average, thirdTestScore, secondAndThirdAveScore);
@@ -16,7 +25,8 @@ int main() {
 
     return 0;
 }
-
+                    //User-define functions definition/s
+//  This function prompts the user to input the test scores, and calculate them.
 void scoresRead(int *test1, int *test2, int *test3, int *average, int *secondAndThirdAveScore) {
     printf("Enter the first test score: ");
     scanf("%d", test1);
@@ -24,10 +34,10 @@ void scoresRead(int *test1, int *test2, int *test3, int *average, int *secondAnd
     scanf("%d", test2);
     printf("Enter the third test score: ");
     scanf("%d", test3);
-    *average = (*test1 + *test2 + *test3) / 3;
-    *secondAndThirdAveScore = (*test2 + *test3) / 2;
+    *average = (*test1 + *test2 + *test3) / 3;      //To get the average you can calculate them by simply adding the 3 test scores then divide them by 3
+    *secondAndThirdAveScore = (*test2 + *test3) / 2;    //This calculation is for the "If the third score is more than 90%" then the grade is A.
 }
-
+//  This function decides what grade will the student get.
 char gradeDecide(int average, int test3, int secondAndThirdAveScore) {
     if (average >= 90) {
         return 'A';
@@ -47,7 +57,7 @@ char gradeDecide(int average, int test3, int secondAndThirdAveScore) {
         return 'F';
     }
 }
-
+//  This functioms prints the output.
 void showOutput(char grade) {
     printf("Congratulations your rating for this grade is : %c\n", grade);
 }
