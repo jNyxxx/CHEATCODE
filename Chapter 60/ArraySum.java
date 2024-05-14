a8a0fb2f46bd29e83cda27b684fb491a8aefdc4b
nYx <147728991+jNyxxx@users.noreply.github.com>
2024-05-15 00:05:15 +0800

    ArraySum.java

---
+++ Chapter 60/ArraySum.java
@@ -0,0 +1,15 @@
+/* Author: Baran, Junex Glenn A.
+ * Date: May 6, 2024
+ */
+public class ArraySum {
+    public static void main(String[] args) {
+        // Declare and initialize an integer array
+        int[] val = {0, 1, 2, 3};
+
+        // Calculate the sum of all numbers in the array
+        int sum = val[0] + val[1] + val[2] + val[3];
+
+        // Print the sum of all numbers
+        System.out.println("Sum of all numbers = " + sum);
+    }
+}
