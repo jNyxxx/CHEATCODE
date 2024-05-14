3408c07c12f5e7214ab5d55e15436dccfa5c717e
nYx <147728991+jNyxxx@users.noreply.github.com>
2024-05-15 00:02:54 +0800

    Add files via upload

---
+++ Chapter 60/TwoArrays.java
@@ -0,0 +1,21 @@
+/* Author: Baran, Junex Glenn A.
+ * Date: May 9, 2024
+ */
+public class TwoArrays {
+    public static void main(String[] args) {
+        int[] val = {13, -4, 82, 17}; 
+        int[] twice;
+    
+    System.out.println( "Original Array: " 
+        + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
+ 
+    // Construct an array object for twice.
+        twice = new int[] {26, -8, 164, 34};  
+
+
+ 
+    System.out.println( "New Array: " 
+        + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3] );
+    }
+    
+}
